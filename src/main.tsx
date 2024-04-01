c90b0cac70f90073b5fd858d5df79f8e926aa916
Nikita <neavinstasi@icloud.com>
2024-04-01 23:51:25 +0300

    add normalize styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
+import 'normalize.css';
+import '@/styles/styles.scss';
+
 const app = ReactDOM.createRoot(document.getElementById('root')!);
 
 const init = () => {
